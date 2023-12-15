aceb66cb8316f6c2ae3f62c2caf6058565f4a40f
Ryutaro <ryutaro9595@gmail.com>
2023-12-16 06:09:52 +0900

    add webscraping and html2markdown

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module scraper-markdown-go
 
 go 1.20
+
+require (
+	github.com/JohannesKaufmann/html-to-markdown v1.4.2
+	github.com/chromedp/chromedp v0.9.3
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/chromedp/cdproto v0.0.0-20231011050154-1d073bb38998 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"net/url"
+	"os"
+	"time"
+
+	md "github.com/JohannesKaufmann/html-to-markdown"
+	"github.com/chromedp/chromedp"
 )
 
 func validateURL(urlStr string) error {
@@ -28,6 +35,50 @@ func validateURL(urlStr string) error {
 	return nil
 }
 
+func getHTML(url string) (string, string, error) {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	var res string
+	var title string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible("article"),
+		chromedp.OuterHTML("article", &res, chromedp.ByQuery),
+		chromedp.Text("h1", &title, chromedp.ByQuery),
+	)
+	if err != nil {
+		return "", "", err
+	}
+	return res, title, nil
+}
+
+func htmlToMarkdown(html string) (string, error) {
+	converter := md.NewConverter("", true, nil)
+	markdown, err := converter.ConvertString(html)
+	if err != nil {
+		return "", err
+	}
+	return markdown, nil
+}
+
+func saveMarkdowntoFile(markdown, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(markdown)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	var url string
 	flag.StringVar(&url, "url", "", "Specify the URL for the tool to interact with.(long form)")
@@ -37,4 +88,20 @@ func main() {
 	if err := validateURL(url); err != nil {
 		log.Fatalf("Invalid URL: %s\n", err)
 	}
+
+	html, title, err := getHTML(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(title)
+	markdown, err := htmlToMarkdown(html)
+	if err != nil {
+		log.Fatalf("err html to markdown: %s\n", err)
+	}
+
+	filename := fmt.Sprintf("%s.md", title)
+	if err := saveMarkdowntoFile(markdown, filename); err != nil {
+		log.Fatalf("failed save markdown: %s\n", err)
+	}
+	fmt.Printf("markdown content has saved to %s\n", filename)
 }
